e011b71df8e173cbf8df7716a618caef999c9e0b
Michael Spiegel <spiagl.m@gmail.com>
2015-10-07 01:16:05 +0200

    Improve tests

--- app/board/board_test.js
+++ app/board/board_test.js
@@ -88,7 +88,7 @@ describe('myApp.board module', function () {
             expect(neighbours).toEqual([right, down]);
         }));
 
-        describe('a protected letter', function() {
+        describe('a protected letter', function () {
 
             var playTopLeftLetters = function ($scope) {
 
@@ -112,11 +112,77 @@ describe('myApp.board module', function () {
                 expect($scope.letter(1, 0).state.protected).toBe(false);
                 expect($scope.letter(0, 1).state.protected).toBe(false);
 
+                for (var x = 1; x < 4; ++x) {
+                    for (var y = 1; y < 4; ++y) {
+                        expect($scope.letter(x, y).state.protected).toBe(false);
+                    }
+                }
+
                 // Test if letter is owned by me
                 expect($scope.letter(0, 0).state.lastPlayedBy).toBe('me');
             };
 
-            it('should be surrounded by letters last played by the same player', inject(function($controller) {
+            var playLETS = function ($scope) {
+
+                // Define letters
+                var L = $scope.letter(0, 1);
+                var E = $scope.letter(0, 0);
+                var T = $scope.letter(1, 0);
+                var S = $scope.letter(2, 0);
+
+                var P = $scope.letter(3, 0);
+                var A = $scope.letter(3, 1);
+                var Y = $scope.letter(1, 2);
+
+                // Define words
+                var LETS = [L, E, T, S];
+                var PLAY = [P, L, A, Y];
+
+                $scope.dictionary = ['lets', 'play'];
+
+                // Me plays LETS
+                LETS.forEach(function (letter) {
+                    $scope.chooseLetter(letter);
+                });
+                expect($scope.chosenWord()).toEqual('lets');
+                //$scope.acceptWord();
+                $scope.submit();
+
+                expect(L.state.protected).toBe(false);
+                expect(E.state.protected).toBe(true);
+                expect(T.state.protected).toBe(false);
+                expect(S.state.protected).toBe(false);
+                expect(P.state.protected).toBe(false);
+                expect(A.state.protected).toBe(false);
+                expect(Y.state.protected).toBe(false);
+
+                // Other plays PLAY
+                PLAY.forEach(function (letter) {
+                    $scope.chooseLetter(letter);
+                });
+                expect($scope.chosenWord()).toEqual('play');
+                $scope.submit();
+
+                expect(L.state.protected).toBe(false);
+                expect(L.state.lastPlayedBy).toBe("other");
+                expect(E.state.protected).toBe(false);
+                expect(E.state.lastPlayedBy).toBe("me");
+                expect(T.state.protected).toBe(false);
+                expect(T.state.lastPlayedBy).toBe("me");
+                expect(S.state.protected).toBe(false);
+                expect(S.state.lastPlayedBy).toBe("me");
+
+                expect(P.state.protected).toBe(false);
+                expect(P.state.lastPlayedBy).toBe('other');
+                expect(A.state.protected).toBe(false);
+                expect(A.state.lastPlayedBy).toBe('other');
+                expect(Y.state.protected).toBe(false);
+                expect(Y.state.lastPlayedBy).toBe('other');
+
+
+            };
+
+            it('should be surrounded by letters last played by the same player', inject(function ($controller) {
                 var $scope = {};
                 var boardCtrl = $controller('BoardCtrl', {$scope: $scope});
 
@@ -147,7 +213,7 @@ describe('myApp.board module', function () {
             }));
 
 
-            it('should keep it owner but become unprotected if a neighbour changes its owner', inject(function ($controller) {
+            it('should keep its owner but become unprotected if a neighbour changes its owner', inject(function ($controller) {
                 var $scope = {};
                 var boardCtrl = $controller('BoardCtrl', {$scope: $scope});
 
@@ -168,6 +234,14 @@ describe('myApp.board module', function () {
 
                 // Test if letter is still owned by me
                 expect($scope.letterRows[0][0].state.lastPlayedBy).toBe('me');
+            }));
+
+            it('should keep its owner but become unprotected if a neighbour changes its owner', inject(function ($controller) {
+                var $scope = {};
+                var boardCtrl = $controller('BoardCtrl', {$scope: $scope});
+
+                playLETS($scope);
+
             }));
 
         });
